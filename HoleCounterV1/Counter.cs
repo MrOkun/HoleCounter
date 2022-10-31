6181446a9d4c10e7620b27f0d49ca3d59eddf823
Alexander Klush <alexanderklush5@gmail.com>
2022-10-31 13:49:43 +0300

    init

---
+++ HoleCounterV1/Counter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace HoleCounterV1
+{
+    public class Counter
+    {
+        public int Count(string input)
+        {
+            int count = 0;
+            string table;
+
+
+            if (!File.Exists("table.json"))
+            {
+
+            }
+
+            using (var stream = new StreamReader("table.json"))
+            {
+                table = stream.ReadToEnd();
+            }
+
+            var json = JObject.Parse("{ \"Data\":" + table + "}");
+
+            var tableObj = json["Data"];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                count += (int)tableObj[input[i].ToString()];
+            }
+
+            return count;
+        }
+    }
+}
---
+++ HoleCounterV1Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HoleCounterV1;
+
+namespace HoleCounterV1Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+
+            var text = Console.ReadLine();
+
+            Console.WriteLine($"text \"{text}\" has {counter.Count(text)} holes.");
+        }
+    }
+}
